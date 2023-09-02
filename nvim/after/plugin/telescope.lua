a145c2fc1cfd4080e96db4e711a8777d5b0c2a7e
Goncalo Brito <gbrito@gdsbb.org>
2023-09-02 17:56:15 +0200

    [REF] Update nvim telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,23 @@
+local telescope = require("telescope")
+local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>ps", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            }
+        }
+    }
+})
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>ff", builtin.find_files, {desc = "Fuzzy find files in project"})
+keymap.set("n", "<leader>fr", builtin.oldfiles, {desc = "Fuzzy find recent files"})
+keymap.set("n", "<leader>fs", builtin.live_grep, {desc = "Fuzzy string in project"})
+keymap.set("n", "<leader>fc", builtin.grep_string, {desc = "Fuzzy string under cursor in project"})
+keymap.set("n", "<leader>vh", builtin.help_tags, {})
