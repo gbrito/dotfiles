5ba138c21f712f1bfc52f9f02d8bc10b06f842ad
Goncalo Brito <gbrito@gdsbb.org>
2023-09-07 08:00:58 +0200

    [REF] add nerd tree

--- nvim/lua/gbrito/packer.lua
+++ nvim/lua/gbrito/packer.lua
@@ -6,6 +6,13 @@ return require('packer').startup(function(use)
   use 'stevearc/dressing.nvim'
 
   use {
+	  'nvim-tree/nvim-tree.lua',
+	  requires = {
+            {'nvim-tree/nvim-web-devicons'},
+        }
+  }
+
+  use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  requires = {
             {'nvim-lua/plenary.nvim'},
