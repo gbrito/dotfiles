7e69927ca5a089002d7c6cad5bca640c570074e5
Goncalo Brito <gbrito@gdsbb.org>
2023-10-05 18:24:49 +0200

    [REF] add tab keymaps

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -1,3 +1,7 @@
 local bufferline = require("bufferline")
 
 bufferline.setup()
+
+vim.keymap.set("n", "<leader>tc", ":BufferLineCloseOthers<CR>")
+vim.keymap.set("n", "<C-n>", ":BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<C-m>", ":BufferLineCycleNext<CR>")
