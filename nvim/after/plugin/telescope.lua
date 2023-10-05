77f56a4b5ce2b831896b0343283cecd88903439d
Goncalo Brito <gbrito@gdsbb.org>
2023-10-05 06:53:59 +0200

    [REF] livegrep with extensions

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,6 +2,8 @@ local telescope = require("telescope")
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
+telescope.load_extension("live_grep_args")
+
 telescope.setup({
     defaults = {
         mappings = {
@@ -18,6 +20,6 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in project" })
 keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Fuzzy string in project" })
+keymap.set("n", "<leader>fs", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "Fuzzy string in project" })
 keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Fuzzy string under cursor in project" })
 keymap.set("n", "<leader>vh", builtin.help_tags, {})
