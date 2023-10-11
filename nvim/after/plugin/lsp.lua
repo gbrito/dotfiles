2b4cb8b514293cab59abbb377e4de6bbcd8dfd71
Goncalo Brito <gbrito@gdsbb.org>
2023-10-11 07:08:08 +0200

    [ADD] beautysh to lsp formatting

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -79,6 +79,7 @@ cmp.setup({
 local null_ls = require("null-ls")
 
 local sources = {
+    null_ls.builtins.formatting.beautysh,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.ruff,
