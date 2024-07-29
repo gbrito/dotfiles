5cdef98df7fa3982b94e792af0a2e0b2cae67e62
Goncalo Brito <gbrito@gdsbb.org>
2024-07-29 20:46:12 +0200

    [ADD] lazygit to configuration

---
+++ nvim/lua/gbrito/lazy/lazygit.lua
@@ -0,0 +1,16 @@
+return {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+    },
+}
