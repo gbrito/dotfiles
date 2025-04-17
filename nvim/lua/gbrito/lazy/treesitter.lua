bed03e489fd672fc4733ec1a82dc88c0efe3120c
Goncalo Brito <gbrito@gdsbb.org>
2025-04-17 07:53:36 +0200

    [REF] update treesitter config with textobjects

--- nvim/lua/gbrito/lazy/treesitter.lua
+++ nvim/lua/gbrito/lazy/treesitter.lua
@@ -2,36 +2,19 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
         { "nvim-treesitter/nvim-treesitter-context" },
+        { "nvim-treesitter/nvim-treesitter-textobjects" },
     },
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "python", "xml",
-            },
-
-            sync_install = false,
-            auto_install = true,
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
-
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "master",
-            },
-        }
-
-        vim.treesitter.language.register("templ", "templ")
-    end
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    opts = {
+        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        indent = {
+            enable = true,
+        },
+    },
 }
